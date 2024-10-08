09451c32253ddd49e156b305cb78a07c1dafc049
AdamPolczynski <adam_polczynski@yahoo.com>
2024-10-09 00:02:32 +0200

    functional client axios example

--- examples/clients/axios/index.ts
+++ examples/clients/axios/index.ts
@@ -1,21 +1,34 @@
 import axios from 'axios';
 import { openAsBlob } from 'fs';
+import path from 'path';
 
 const PORT = process.argv[2] ? parseInt(process.argv[2]) : 3000;
 
-const formData = new FormData();
-formData.append('text', 'read me or');
-formData.append('text', 'i will push the button');
-formData.append('some other text [123] {}', 'what a mess');
-formData.append('file', await openAsBlob('../assets/person1.jpg'));
+(async () => {
+  const formData = new FormData();
+  formData.append('text', 'read me or');
+  formData.append('text', 'i will push the button');
+  formData.append('text', 'rumbling');
+  formData.append('some other text [123] {}', 'what a mess');
 
-axios
-  .post(`http://localhost:${PORT}/upload-file`, {
-    body: formData,
-  })
-  .then((res) => {
-    console.warn(res);
-  })
-  .catch((e) => {
-    process.exit(e);
-  });
+  const person1FilePath = path.join(__dirname, '../../assets/person1.jpg');
+  const person2FilePath = path.join(__dirname, '../../assets/person2.jpg');
+  const person3FilePath = path.join(__dirname, '../../assets/person3.jpg');
+  const blob1 = await openAsBlob(person1FilePath);
+  const blob2 = await openAsBlob(person2FilePath);
+  const blob3 = await openAsBlob(person3FilePath);
+
+  formData.append('file', blob1);
+  formData.append('file', blob2);
+  formData.append('file', blob3);
+
+  axios
+    .post(`http://localhost:${PORT}/upload-file`, formData)
+    .then((res) => {
+      console.warn(res);
+    })
+    .catch((e) => {
+      console.debug(e);
+      process.exit(1);
+    });
+})();
